73b8eb90c92277a20ba6159273a0db9429522266
hocinebno3131-source <hocinebno3131@gmail.com>
2026-01-29 10:59:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def verify_account():
         reader = csv.DictReader(f, delimiter=';')
         for row in reader:
             if row['CCP'] == ccp:
-                return render_template('success.html', employee=row, is_admin=is_admin)
+            return render_template('success.html', employee=row, is_admin=True)
 
     return render_template('verify_account.html', message="رقم الحساب غير موجود", is_admin=is_admin)
 
@@ -77,8 +77,8 @@ def success():
         reader = csv.DictReader(f, delimiter=';')
         for row in reader:
             if row['CCP'] == ccp:
-                return render_template('success.html', employee=row, is_admin=is_admin)
-    return render_template('verify_account.html', message="رقم الحساب غير موجود", is_admin=is_admin)
+          return render_template('success.html', employee=row, is_admin=True)
+     return render_template('verify_account.html', message="رقم الحساب غير موجود", is_admin=is_admin)
 
 # تعديل بيانات الموظف (الإدمن)
 @app.route('/edit', methods=['GET', 'POST'])
